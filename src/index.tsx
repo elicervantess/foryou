a1b0a55550eb35a79496f83074c57bb2fe2f7653
eliseocervantes <jireh.cervantes@utec.edu.pe>
2025-02-06 14:46:42 -0500

    jajas foryou

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { AuthProvider } from "./AuthContext"; // Asegúrate de que la ruta sea correcta
 
 ReactDOM.render(
   <React.StrictMode>
-    <AuthProvider>
       <App />
-    </AuthProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
